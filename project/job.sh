03874630c85f9677ffeff39887e248d425cf1b30
giorgiodaneri <giorgio1.daneri@gmail.com>
2024-06-04 13:46:17 +0000

    Update

--- project/job.sh
+++ project/job.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -l
 #SBATCH --cpus-per-task=1                                           # Set the number of CORES per task
-#SBATCH --qos=default                                               # SLURM qos
 #SBATCH --nodes=1                                                   # number of nodes
 #SBATCH --ntasks=2                                                  # number of tasks
 #SBATCH --ntasks-per-node=2                                         # number of tasks per node
