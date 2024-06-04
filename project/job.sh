5141ff72e3a874493778b4188e618484a2f44761
giorgiodaneri <giorgio1.daneri@gmail.com>
2024-06-04 13:30:39 +0000

    Now it should work

--- project/job.sh
+++ project/job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
-#SBATCH --cpus-per-task=1                                            # Set the number of CORES per task
-#SBATCH --qos=default                                                # SLURM qos
+#SBATCH --cpus-per-task=1                                           # Set the number of CORES per task
+#SBATCH --qos=default                                               # SLURM qos
 #SBATCH --nodes=1                                                   # number of nodes
-#SBATCH --ntasks=2                                                   # number of tasks
-#SBATCH --ntasks-per-node=2                                        # number of tasks per node
-#SBATCH --time=01:00:00                                              # time (HH:MM:SS)
-#SBATCH --partition=g100_usr_prod                                           # partition
-#SBATCH --account=a08trb66                                            # project account
+#SBATCH --ntasks=2                                                  # number of tasks
+#SBATCH --ntasks-per-node=2                                         # number of tasks per node
+#SBATCH --time=01:00:00                                             # time (HH:MM:SS)
+#SBATCH --partition=g100_usr_prod                                   # partition
+#SBATCH --account=tra24_sepolimi                                    # project account
 
 singularity run ../my_project.sif
