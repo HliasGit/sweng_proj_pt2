f43d45c07e9bd18bf7e1499e56b2efbd6762e94e
giorgiodaneri <giorgio1.daneri@gmail.com>
2024-06-05 13:00:25 +0200

    Update the job.sh batch file

--- project/job.sh
+++ project/job.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -l
+#SBATCH --job-name=cluster_project                                  # Job name
 #SBATCH --cpus-per-task=1                                           # Set the number of CORES per task
 #SBATCH --nodes=1                                                   # number of nodes
 #SBATCH --ntasks=2                                                  # number of tasks
@@ -6,5 +7,7 @@
 #SBATCH --time=01:00:00                                             # time (HH:MM:SS)
 #SBATCH --partition=g100_usr_prod                                   # partition
 #SBATCH --account=tra24_sepolimi                                    # project account
+#SBATCH -o %x-%j-out.txt                                            # output file name
+#SBATCH -e %x-%j-err.txt                                            # error file name
 
-singularity run ../cluster_project.sif
+singularity run -C cluster_project.sif 
